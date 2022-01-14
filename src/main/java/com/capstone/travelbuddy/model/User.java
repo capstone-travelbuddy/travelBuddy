1c1451dff764663aa809aeb1e62d7ddaee37382d
Bosch Leith <bosch.leith@gmail.com>
2022-01-14 15:52:22 -0600

    Refactored: "users" to "user" table

--- src/main/java/com/capstone/travelbuddy/model/User.java
+++ src/main/java/com/capstone/travelbuddy/model/User.java
@@ -3,7 +3,7 @@ package com.capstone.travelbuddy.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
