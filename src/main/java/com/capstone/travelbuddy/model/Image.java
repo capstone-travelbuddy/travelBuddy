567d121f31405a11c9f34e87a59443bb375ce863
Bosch Leith <bosch.leith@gmail.com>
2022-01-18 09:28:20 -0600

    Added: Getters and Setters

--- src/main/java/com/capstone/travelbuddy/model/Image.java
+++ src/main/java/com/capstone/travelbuddy/model/Image.java
@@ -18,4 +18,36 @@ public class Image {
 	@ManyToOne
 	@JoinColumn(name = "review_id")
 	private Review review;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public Review getReview() {
+		return review;
+	}
+
+	public void setReview(Review review) {
+		this.review = review;
+	}
 }
