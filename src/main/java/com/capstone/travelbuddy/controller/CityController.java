46e0f53bac58f8d0d6e718bd830b74a5a99443e1
Bosch Leith <88731739+BoschLeith@users.noreply.github.com>
2022-01-20 16:37:51 -0600

    Added: dynamic city page view

--- src/main/java/com/capstone/travelbuddy/controller/CityController.java
+++ src/main/java/com/capstone/travelbuddy/controller/CityController.java
@@ -1,13 +1,22 @@
 package com.capstone.travelbuddy.controller;
 
+import com.capstone.travelbuddy.repository.CityRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CityController {
 
+	private CityRepository cityDao;
+
+	public CityController(CityRepository cityDao){
+		this.cityDao = cityDao;
+	}
+
 	@GetMapping("/destinations")
-	public String getCitiesView(){
+	public String getCitiesView(Model model){
+		model.addAttribute("cities", cityDao.findAll());
 		return "cities";
 	}
 
@@ -34,6 +43,11 @@ public class CityController {
 	@GetMapping("/shop")
 	public String getShopView(){
 		return "shop";
+	}
+
+	@GetMapping("/create/review")
+	public String getCreateReview(){
+		return "leaveReview";
 	}
 
 }
