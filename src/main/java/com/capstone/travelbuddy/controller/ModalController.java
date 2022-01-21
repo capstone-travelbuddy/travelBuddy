19216d2e63f72035a308df83988832453b2d3e9e
Adamina Flores <adaminaflores19@gmail.com>
2022-01-21 09:28:58 -0600

    Created: a modal controller

--- src/main/java/com/capstone/travelbuddy/controller/ModalController.java
+++ src/main/java/com/capstone/travelbuddy/controller/ModalController.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("modals")
 public class ModalController {
-    @GetMapping("modals1")
-    public String modal1(){
-        return "modal1";
+
     }
 
-}
+
