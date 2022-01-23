df976432b3c195bd05310352ba2b6760d541c6b1
Bosch Leith <88731739+BoschLeith@users.noreply.github.com>
2022-01-23 17:49:01 -0600

    Added: Repository annotation

--- src/main/java/com/capstone/travelbuddy/repository/ReviewRepository.java
+++ src/main/java/com/capstone/travelbuddy/repository/ReviewRepository.java
@@ -2,6 +2,8 @@ package com.capstone.travelbuddy.repository;
 
 import com.capstone.travelbuddy.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 }
--- src/main/java/com/capstone/travelbuddy/repository/ShopRepository.java
+++ src/main/java/com/capstone/travelbuddy/repository/ShopRepository.java
@@ -2,11 +2,11 @@ package com.capstone.travelbuddy.repository;
 
 import com.capstone.travelbuddy.model.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ShopRepository extends JpaRepository<Shop, Integer> {
-	List<Shop> getShopsByCityId(int id);
 	List<Shop> getShopsByCityIdAndCategoryType(int cityId, String categoryType);
 }
