0a5dd9e540de20746620df8071661bf3fda4b772
Bosch Leith <88731739+BoschLeith@users.noreply.github.com>
2022-01-26 12:33:14 -0600

    Added: authenticated user

--- src/main/java/com/capstone/travelbuddy/controller/ProfileController.java
+++ src/main/java/com/capstone/travelbuddy/controller/ProfileController.java
@@ -1,17 +1,28 @@
 package com.capstone.travelbuddy.controller;
 
+import com.capstone.travelbuddy.model.User;
+import com.capstone.travelbuddy.repository.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProfileController {
 
-    @GetMapping("/profile/{name}")
-    public String profile(@PathVariable String name, Model model) {
-        model.addAttribute("name", name);
+    private UserRepository userDao;
+
+    public ProfileController(UserRepository userDao){
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", userDao.getById(user.getId()));
+
         return "profile";
     }
+
 }
 
