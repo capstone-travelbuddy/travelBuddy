e66e4df62730fc98ecee39da597ce49ae2d62f99
Adamina Flores <adaminaflores19@gmail.com>
2022-01-27 16:58:25 -0600

    added array list for search

--- src/main/java/com/capstone/travelbuddy/repository/ShopRepository.java
+++ src/main/java/com/capstone/travelbuddy/repository/ShopRepository.java
@@ -2,6 +2,7 @@ package com.capstone.travelbuddy.repository;
 
 import com.capstone.travelbuddy.model.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 @Repository
 public interface ShopRepository extends JpaRepository<Shop, Integer> {
 	List<Shop> getShopsByCityIdAndCategoryType(int cityId, String categoryType);
+	List<Shop> findByNameIgnoreCaseContaining(String name);
+
 }
