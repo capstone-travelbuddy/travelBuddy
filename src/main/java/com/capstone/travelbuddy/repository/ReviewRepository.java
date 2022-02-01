fbf8108db0e74543da39cfe3a98db28e594bfbc4
Bosch Leith <88731739+BoschLeith@users.noreply.github.com>
2022-02-01 09:41:09 -0600

    Added: shops average review rating functionality

--- src/main/java/com/capstone/travelbuddy/repository/ReviewRepository.java
+++ src/main/java/com/capstone/travelbuddy/repository/ReviewRepository.java
@@ -2,8 +2,11 @@ package com.capstone.travelbuddy.repository;
 
 import com.capstone.travelbuddy.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+	@Query(value = "SELECT avg(rating) FROM Review WHERE shop.id = ?1")
+	Double avg(int id);
 }
