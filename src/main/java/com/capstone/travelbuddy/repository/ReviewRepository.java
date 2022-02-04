8f0057f5db3b4ce348861903ef864776dddffd6d
Bosch Leith <88731739+BoschLeith@users.noreply.github.com>
2022-02-04 09:41:37 -0600

    Added: rounding to rating average/styled buttons

--- src/main/java/com/capstone/travelbuddy/repository/ReviewRepository.java
+++ src/main/java/com/capstone/travelbuddy/repository/ReviewRepository.java
@@ -10,9 +10,28 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
 	@Query(value = "SELECT avg(rating) FROM Review WHERE shop.id = ?1")
 	Double avg(int id);
 
+	default double avgRound(int precision, int id) {
+		Double value = avg(id);
+		int scale = (int) Math.pow(10, precision);
+		return (double) Math.round(value * scale) / scale;
+	}
+
 	@Query(value = "SELECT avg(rating) FROM Review WHERE shop.id = ?1 AND userType = 1")
 	Double localAvg(int id);
 
+	default double localAvgRound(int precision, int id) {
+		Double value = localAvg(id);
+		int scale = (int) Math.pow(10, precision);
+		return (double) Math.round(value * scale) / scale;
+	}
+
 	@Query(value = "SELECT avg(rating) FROM Review WHERE shop.id = ?1 AND userType = 2")
 	Double travelerAvg(int id);
+
+	default double travelerAvgRound(int precision, int id) {
+		Double value = travelerAvg(id);
+		int scale = (int) Math.pow(10, precision);
+		return (double) Math.round(value * scale) / scale;
+	}
+
 }
